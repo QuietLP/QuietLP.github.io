417085de3d5a0b3b04524f27cf7bf7784a6632cf
QuietLP <teamsonorus@gmail.com>
2024-11-18 22:22:23 -0500

    Actualizacion Final
    Ultima modificacion del Landing Page para la entrega

---
+++ oscuro.js
@@ -0,0 +1,18 @@
+const themeSwitch = document.querySelector("#switch-label");
+themeSwitch.addEventListener("change", () => {
+  document.body.classList.toggle("dark-mode");
+
+  if (document.body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme === "dark") {
+    document.body.classList.add("dark-mode");
+    themeSwitch.checked = true; // Activa el interruptor
+  }
+});
